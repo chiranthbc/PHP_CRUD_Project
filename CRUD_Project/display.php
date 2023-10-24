36d46b4de606df11cf9ced5de32b297560e1caf5
chiranthbc <chiranthbc1@gmail.com>
2023-10-25 01:05:53 +0530

    second commit

--- CRUD_Project/display.php
+++ CRUD_Project/display.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+    <h1>PHP CRUD Operation</h1>
     <div class="container my-5">
         <button class="btn btn-primary"><a href="user.php" class="text-light">Add User</a> </button>
     </div>
